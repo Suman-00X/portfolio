0e233a316a95ff47d08615f52fee7be6d37f2eac
Suman raj <raj_suman@hotmail.com>
2022-04-11 08:24:09 +0530

    ready to deploy

--- portf1/urls.py
+++ portf1/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.views.static import serve
+# from django.conf.urls import urls
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
@@ -26,4 +30,9 @@ urlpatterns = [
     path('resume', views.resume),
     path('education', views.education),
     path('about', views.about)
+
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+ 
